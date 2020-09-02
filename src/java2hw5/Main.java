1d7a06fa71d770534abbe53e66bfdb491a609b94
yelzh <11l.yelzhank@gmail.com>
2020-09-02 23:52:05 +0600

    Java2 HW 5.1

--- src/java2hw5/Main.java
+++ src/java2hw5/Main.java
@@ -6,41 +6,51 @@ public class Main {
     static final int size = 10000000;
     static final int h = size / 2;
 
-    static float[] arr = new float[size];
-    static float[] a1 = new float[h];
-    static float[] a2 = new float[h];
-
     public static void main(String[] args) {
+        singleThread();
+        doubleThread();
+    }
+
+    public static void singleThread() {
+        float[] arr = new float[size];
         Arrays.fill(arr, 1);
+        long time = System.currentTimeMillis();
+        calculateArray(arr);
+        System.out.println("Single Thread Time: " + (System.currentTimeMillis() - time) + " ms");
+    }
 
-        long a = System.currentTimeMillis();
+    public static void doubleThread() {
+        float[] arr = new float[size];
+        float[] a1 = new float[h];
+        float[] a2 = new float[h];
+        Arrays.fill(arr, 1);
+        long time = System.currentTimeMillis();
 
-        Thread t1 = new Thread(() -> {
-            System.arraycopy(arr, 0, a1, 0, h);
-            calArray(a1);
-            System.arraycopy(a1, 0, arr, 0, h);
-            System.out.println("Time Thread 1: " + (System.currentTimeMillis() - a) + " ms");
-        });
+        System.arraycopy(arr, 0, a1, 0, h);
+        System.arraycopy(arr, h, a2, 0, h);
 
-        Thread t2 = new Thread(() -> {
-            System.arraycopy(arr, h, a2, 0, h);
-            calArray(a2);
-            System.arraycopy(a2, 0, arr, h, h);
-            System.out.println("Time Thread 2: " + (System.currentTimeMillis() - a) + " ms");
-        });
+        Thread t1 = new Thread(() -> calculateArray(a1));
+        Thread t2 = new Thread(() -> calculateArray(a2));
 
         t1.start();
         t2.start();
 
-        calArray(arr);
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
-        System.out.println("Time: " + (System.currentTimeMillis() - a) + " ms");
+        System.arraycopy(a1, 0, arr, 0, h);
+        System.arraycopy(a2, 0, arr, h, h);
+
+        System.out.println("Double Thread Time: " + (System.currentTimeMillis() - time) + " ms");
     }
 
-    public static void calArray(float[] newarr){
-
-        for (int i = 0; i < newarr.length; i++) {
-            newarr[i] = (float) (newarr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+    public static void calculateArray(float[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
         }
     }
 }
