2755838467bdc16b8bc364b3391efdcdcf3e9a8a
yelzh <11l.yelzhank@gmail.com>
2020-09-09 23:48:07 +0600

    Java2 HW 7.0

---
+++ src/java2hw7/server/ClientHandler.java
@@ -0,0 +1,95 @@
+package java2hw7.server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Arrays;
+
+public class ClientHandler {
+    private Server server;
+    private Socket socket;
+    private DataInputStream in;
+    private DataOutputStream out;
+
+    private String nickname;
+
+    public ClientHandler(Server server, Socket socket) {
+        try {
+            this.server = server;
+            this.socket = socket;
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+
+            new Thread(() -> {
+                try {
+                    //цикл аутентификации
+                    while (true) {
+                        String str = in.readUTF();
+
+                        if (str.startsWith("/auth ")) {
+                            String[] token = str.split("\\s");
+                            String newNick = server
+                                    .getAuthService()
+                                    .getNicknameByLoginAndPassword(token[1], token[2]);
+
+                            if (newNick != null) {
+                                nickname = newNick;
+                                sendMsg("/authok " + nickname);
+                                server.subscribe(this);
+                                System.out.println("Клиент " + nickname + " подключился");
+                                break;
+                            } else {
+                                sendMsg("Неверный логин / пароль");
+                            }
+                        }
+                    }
+
+                    //цикл работы
+                    while (true) {
+                        String str = in.readUTF();
+
+                        if (str.equals("/end")) {
+                            out.writeUTF("/end");
+                            break;
+                        } else if (str.startsWith("/w ")) {
+                            String[] message = str.split(" ");
+                            String friendname = message[1];
+                            String s = str.substring(3 + friendname.length());
+
+                            server.broadcastMsg(this, s, friendname);
+                            out.writeUTF(nickname + " : " + str);
+                        } else {
+                            server.broadcastMsg(this, str);
+                        }
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    System.out.println("Клиент отключился");
+                    server.unsubscribe(this);
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    void sendMsg(String msg) {
+        try {
+            out.writeUTF(msg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+}
